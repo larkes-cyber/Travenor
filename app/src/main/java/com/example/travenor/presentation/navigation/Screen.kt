a14e1f9e743b7c9c387d9d3841f8e0d1e14b0235
Your Name <playegrovoy@gmail.com>
2022-11-30 16:44:31 +0300

    fix

---
+++ app/src/main/java/com/example/travenor/presentation/navigation/Screen.kt
@@ -0,0 +1,20 @@
+package com.example.travenor.presentation.navigation
+
+sealed class Screen(val route:String){
+    object SplashScreen: Screen("splash_screen")
+    object TrailerScreen: Screen("trailer_screen")
+    object AuthScreen: Screen("auth_screen")
+    object RegScreen: Screen("reg_screen")
+    object ForgotPassword: Screen("forgot_password")
+
+    fun withArgs(vararg args: String):String{
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+}
+
